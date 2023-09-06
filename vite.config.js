f7f8a22f24adfbaa8b48acd51febfd46b3e77145
nocive.science <nocive.science@gmail.com>
2023-09-06 05:57:51 -0300

    termindo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/nocivescience/reactFirstExample/tree/main/src/frontend',
+  base: 'https://github.com/nocivescience/frontend',
 })
